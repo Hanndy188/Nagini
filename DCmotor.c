668896c6e4b738f7b3f86d245927303a8d387b48
Hanndy188 <62260827+Hanndy188@users.noreply.github.com>
2020-04-02 21:44:22 +0200

    Create DCmotor.c

---
+++ DCmotor.c
@@ -0,0 +1,11 @@
+7* Motor will stop when the distance to the object is 0.5cm.
+   motor will rotate when the distance to the object is between 0.5cm and 300cm.
+   motor will start when a switch is on.
+*/
+const int motorDistance = 0.5 cm
+const int switchState == ON
+
+void main {
+          motorStop( 1, 0, 0.5 );
+          motorRun( 1, 100, 300 );
+          }
